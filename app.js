ae1c9e6d7a3d1bbb656eb893f7935cde0227cda1
FRED NYAKAGWA <fnyakagwa@mo-de.local>
2020-09-01 12:24:20 +0300

    added procfile, gitignore, package.json, app.js and server.js for heroku deployment testing

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use((req, res, next) => {
+  console.log('Request received!');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Your request was successful!' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Response sent successfully!');
+});
+
+module.exports = app;
